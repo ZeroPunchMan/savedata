2598d531b1931c474982522802937efa4bb3ac95
lyj <knightlyj@163.com>
2023-08-06 23:10:11 +0800

    bat

--- save.bat
+++ save.bat
@@ -1,2 +1,5 @@
 rd /s /q "D:\\Data\\savedata\\MHRS\\win64_save"
 echo D | xcopy "C:\\Program Files (x86)\\Steam\\userdata\\339055594\\1446780\\remote\\win64_save" "D:\\Data\\savedata\\MHRS\\win64_save" /E /H
+
+rd /s /q "D:\\Data\\savedata\\BL3\\SaveGames"
+echo D | xcopy "C:\\Users\\lyj\\Documents\\My Games\\Borderlands 3\\Saved\\SaveGames" "D:\\Data\\savedata\\BL3\\SaveGames" /E /H
